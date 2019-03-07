7b93cd88ba558d374e50d7e9b9c671a1e853fbe0
dan <xiexyz@gmail.com>
2019-03-07 22:32:13 +0800

    update

--- preinstall.sh
+++ preinstall.sh
@@ -12,7 +12,7 @@ systemctl start docker
 systemctl enable docker
 
 curl -L \
-   https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` \
+   https://get.daocloud.io/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` \
    -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
