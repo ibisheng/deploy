0532caf2493712d3031aadb92cc9a6e2ed627aeb
dan <xiexyz@gmail.com>
2019-03-08 11:01:26 +0800

    net work

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+if [ ! -n "$1" ] ;then
+    echo "初始化类型"
+    exit
+fi
+if [ ! -n "$2" ] ;then
+    echo "必须指定容器标签"
+    exit
+fi
+docker rm tools -f
+
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:${tag}
+
+docker run --name tools --privileged  \
+    -v /data:/workspace \
+    -e RUN_ENV=docker \
+    -e type=$1  \
+    --net=bisheng  \
+    registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:$2
