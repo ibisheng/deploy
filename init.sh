6a9dd1da1c35776e7d90010c980fdfcb746bb416
dan <xiexyz@gmail.com>
2019-03-08 11:19:28 +0800

    net work

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ if [ ! -n "$2" ] ;then
 fi
 docker rm tools -f 1 > /dev/null 2>&1
 
-docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:${tag}
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:$2
 
 docker run --name tools --privileged  \
     -v /data:/workspace \
