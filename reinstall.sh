cf514dae8379210bfe0f20c660843b554a5b2893
dan <xiexyz@gmail.com>
2019-03-12 21:19:32 +0800

    update

--- reinstall.sh
+++ reinstall.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-docker rm nginx minio dgraphServer dgraphZero redis dgraphRatel rabbit search mongod  drive_full editor_app convert editor -f  1 > /dev/null 2>&1
+docker rm nginx 1 > /dev/null 2>&1
+docker rm drive_full editor_app convert editor -f  1 > /dev/null 2>&1
+docker rm minio dgraphServer dgraphZero redis dgraphRatel rabbit search mongod  1 > /dev/null 2>&1
 docker network create bisheng 1 > /dev/null 2>&1
 
 sh upgrade.sh
