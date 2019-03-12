36d53edb197bb4c7209976b06112edf825c887ea
dan <xiexyz@gmail.com>
2019-03-12 21:28:15 +0800

    update

--- reinstall.sh
+++ reinstall.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "remove container nginx"
 docker rm nginx -f 1 > /dev/null 2>&1
-echo "remove service "
+echo "remove apps "
 docker rm drive_full editor_app convert editor -f  1 > /dev/null 2>&1
-echo "remove apps"
+echo "remove service"
 docker rm minio dgraphServer dgraphZero redis dgraphRatel rabbit search mongod -f  1 > /dev/null 2>&1
 docker network create bisheng 1 > /dev/null 2>&1
 
