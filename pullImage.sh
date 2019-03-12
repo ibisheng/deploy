b51aa5a9861fca16e152db9776085d62d3b40296
dan <xiexyz@gmail.com>
2019-03-12 17:45:20 +0800

    net work

--- pullImage.sh
+++ pullImage.sh
@@ -5,3 +5,9 @@ docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/editor:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/convert:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/search:free
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:${tag}
+
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/dgraph:v1.0.5
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/mongo:3.6
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/redis:4.0
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/rabbitmq:3.6
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/minio:latest
