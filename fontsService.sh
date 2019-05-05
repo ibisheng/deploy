7b1e3f5e487cee261f40173a1f09750944bad379
dan <xiexyz@gmail.com>
2019-05-05 10:25:54 +0800

    remove fonts file

--- fontsService.sh
+++ fontsService.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+if [ ! -d userFonts  ];then
+  mkdir userFonts
+fi
+if [  -n "$1" ] ;then
+    echo "你指定了字体目录，请确保该目录下有字体文件"
+    cp $1/* userFonts
+fi
+
+
 var=$(cat .config)
 arr=()
 for element in $var
@@ -12,6 +22,8 @@ tag=${arr[1]}
 rm -rf $data/workspace/fonts/*
 
 cp -r workspace/fonts/* $data/workspace/fonts
+cp -r userFonts/* $data/workspace/fonts
+
 
 bash init.sh 64 $tag $data
 
