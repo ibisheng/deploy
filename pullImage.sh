e1164c19a78def22fd70237679b19532d86d6066
xiedan <xiedan@gmail.com>
2020-08-17 23:10:02 +0800

    修改内容

--- pullImage.sh
+++ pullImage.sh
@@ -2,11 +2,9 @@
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/drive_full:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/editor_app:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/editor:$1
-docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/convert:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:$1
 
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/dgraph:v1.0.5
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/mongo:3.6
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/redis:4.0
-docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/rabbitmq:3.6
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/minio:latest
