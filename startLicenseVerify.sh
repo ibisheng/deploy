9c5b0993f4946d4e0f35d9e660d4829a1258ed53
dan <dan@dandeMBP-3.lan>
2020-10-07 12:35:44 +0800

    license verify

--- startLicenseVerify.sh
+++ startLicenseVerify.sh
@@ -4,7 +4,7 @@ PROC_NAME=licenseVerify
 ProcNumber=`ps -ef |grep -w $PROC_NAME|grep -v grep|wc -l`
 if [ $ProcNumber -le 0 ];then
    echo "licenseVerify is not run"
-   sh ./licenseVerify
+   ./licenseVerify
 else
    echo "licenseVerify is  running.."
 fi
