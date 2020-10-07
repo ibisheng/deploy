d6e79f1419fbcbc295c9c1308dfe5a727d4abc18
dan <dan@dandeMBP-3.lan>
2020-10-07 12:34:39 +0800

    license verify

--- startLicenseVerify.sh
+++ startLicenseVerify.sh
@@ -4,7 +4,7 @@ PROC_NAME=licenseVerify
 ProcNumber=`ps -ef |grep -w $PROC_NAME|grep -v grep|wc -l`
 if [ $ProcNumber -le 0 ];then
    echo "licenseVerify is not run"
-   sh /root/shell/docker-start.sh
+   sh ./licenseVerify
 else
    echo "licenseVerify is  running.."
 fi
