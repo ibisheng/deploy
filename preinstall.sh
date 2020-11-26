395e07aa87c11cef9e351ea04d526c6ff0c5c6ab
毕升Office <xiehongan123@163.com>
2020-11-26 11:44:36 +0800

    update preinstall.sh.

--- preinstall.sh
+++ preinstall.sh
@@ -11,8 +11,6 @@ yum install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker
 systemctl enable docker
 
-curl -L \
-   https://get.daocloud.io/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` \
-   -o /usr/local/bin/docker-compose
-
+wget  https://github.com/docker/compose/releases/download/1.25.4/docker-compose-Linux-x86_64
+mv docker-compose-Linux-x86_64  >> /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
