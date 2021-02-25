bded03adc82fa6cbea611bb8ac9d51a7131eb341
dan <dan@dandeMBP-3.lan>
2021-02-25 18:45:23 +0800

    增加convert节点

--- pullImage.sh
+++ pullImage.sh
@@ -2,6 +2,7 @@
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/drive_full:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/editor_app:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/editor:$1
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/convert:$1
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/tools:$1
 
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/dgraph:v1.0.5
