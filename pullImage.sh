160619b9a53fd790985cfa8122c3021eba4e0e1e
dan <dan@dandeMBP-3.lan>
2023-06-09 17:12:21 +0800

    v3.1分支安装

--- pullImage.sh
+++ pullImage.sh
@@ -2,5 +2,6 @@
 docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/editor_app:$1
 
 
-docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/redis:6.2.8
-docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/minio:latest
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/rejson:2.0.9
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/minio:RELEASE.2021-10-10T16-53-30Z
+docker pull registry.cn-zhangjiakou.aliyuncs.com/bisheng/mysql:8-oracle
